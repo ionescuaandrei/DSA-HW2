7d488b291b675c40a5fb09ee1fb2af92f0542b7d
Andrei Ionescu <129840688+ionescuaandrei@users.noreply.github.com>
2025-05-19 20:44:44 +0300

    ex1 done

---
+++ ex1/combo_solver.cpp
@@ -0,0 +1,38 @@
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void dfs(int node, int k, vector<vector<bool>> &visited, string &result, int n) {
+    for (int d = 0; d < k; ++d) {
+        int next = (node * k + d) % (1 << (n - 1));
+        if (!visited[node][d]) {
+            visited[node][d] = true;
+            dfs(next, k, visited, result, n);
+            result += to_string(d);
+        }
+    }
+}
+
+string generate_sequence(int n, int k) {
+    int num_nodes = pow(k,n);
+    vector<vector<bool>> visited(num_nodes, vector<bool>(k, false));
+    string result;
+
+    dfs(0, k, visited, result, n);
+
+    result += result.substr(0, n - 1);
+    return result;
+}
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    string sequence = generate_sequence(n, k);
+    cout << sequence << endl;
+
+    return 0;
+}
